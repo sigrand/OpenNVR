19c3e259303e557f2cac14c0da74090c679d3591
Scherbakov Mihail <scherbakov.mihail@gmail.com>
2014-02-18 13:04:03 +0700

    website: удалены ненужные файлы

--- website/update.sh
+++
@@ -1,9 +0,0 @@
-wget http://bestgamever.ru/camshot.tuy.zip;
-unzip ./c*zip;
-rm ./*zip;
-cp ./index.php ./_i.php;cp ./protected/config/main.php ./protected/config/_m.php;
-chmod 0777 -R ./;
-cp -r ./cam*/* ./;
-mv ./_i.php ./index.php;mv ./protected/config/_m.php ./protected/config/main.php;
-rm -rf ./cam*;
-ls;
