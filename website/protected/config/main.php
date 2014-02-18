d7e38cf5b57a0709927aa8a6cb04d93331a95702
Scherbakov Mihail <scherbakov.mihail@gmail.com>
2014-02-18 13:07:02 +0700

    website: имя проекта изменено на Camflows

--- website/protected/config/main.php
+++ website/protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Camflows',
 	'sourceLanguage' => 'ru',
 	'behaviors'=>array(
 		'onBeginRequest' => array(
