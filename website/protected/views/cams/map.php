7a41c3e5b8a5300a00ef368be0a64992743223f4
Scherbakov Mihail <scherbakov.mihail@gmail.com>
2014-02-20 18:03:09 +0700

    website: автоматическое растягивание видео в плеерах карусели

--- website/protected/views/cams/map.php
+++ website/protected/views/cams/map.php
@@ -57,7 +57,7 @@
                 <param name="bgcolor" value="#000000"/>
                 <param name="allowFullScreen" value="true"/>
                 <param name="FlashVars" value="autoplay=0&playlist=1&buffer=0.3"/>
-                <embed FlashVars="autoplay=0&playlist=1&buffer=0.3"
+                <embed FlashVars="autoplay=0&playlist=1&buffer=0.3&auto_horizontal_mode=true"
                 src="<?php echo Yii::app()->request->baseUrl; ?>/player/MyPlayer_hi_lo.swf"
                 bgcolor="#000000"
                 width="100%"
